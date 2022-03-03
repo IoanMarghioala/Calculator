6ddb3ece1d1490bb464e390118d34f9f111020e6
IMARGHIO <ioan.marghioala@capgemini.com>
2022-03-03 16:16:12 +0200

    Update Calculator

--- src/equations/Equation.java
+++ src/equations/Equation.java
@@ -5,42 +5,77 @@ import java.util.List;
 
 public class Equation {
     public static void main(String[] args) {
-        if (args.length == 3) {
-            int firstNumber = 0;
-            int secondNumber = 0;
-            try{
-                firstNumber = Integer.parseInt(args[0]);
-                secondNumber = Integer.parseInt(args[2]);
-            } catch (NumberFormatException numberFormatException){
-                System.out.println("Unul dintre parametrii nu este numar");
-            }
-            int result = 0;
-            String commandOperator = args[1];
-            List<String> operators = Arrays.asList("+","-","*","/");
-            if (operators.stream().anyMatch(operator -> operator.equals(commandOperator))) {
-                switch (commandOperator) {
-                    case "+":
-                        result = firstNumber + secondNumber;
-                        break;
-                    case "-":
-                        result = firstNumber - secondNumber;
-                        break;
-                    case "*":
-                        result = firstNumber * secondNumber;
-                        break;
-                    case "/":
-                        if (secondNumber != 0) {
-                            result = firstNumber / secondNumber;
-                        } else {
-                            System.out.println("Nu se poate efectua impartirea la 0");
-                        }
-                        break;
+
+        int firstNumber = 0;
+        int secondNumber = 0;
+        int result = 0;
+        String commandOperator = args[1];
+
+        if (args.length != 3) {
+            System.exit(0);
+            System.out.println("You need to introduce 3 parameters.");
+        }
+
+        if (!checkIfOperatorIsCorrect(commandOperator)) {
+            System.exit(0);
+            System.out.println("Your parameters is not correct");
+        }
+
+        if (checkIfParametersAreNumbers(args[0]) && checkIfParametersAreNumbers(args[2])) {
+            firstNumber = Integer.parseInt(args[0]);
+            secondNumber = Integer.parseInt(args[2]);
+        }
+
+        switch (commandOperator) {
+            case "+":
+                result = firstNumber + secondNumber;
+                break;
+            case "-":
+                result = firstNumber - secondNumber;
+                break;
+            case "*":
+                result = firstNumber * secondNumber;
+                break;
+            case "/":
+                if (secondNumber != 0) {
+                    result = firstNumber / secondNumber;
+                } else {
+                    System.out.println("You can't divide by 0.");
                 }
-            }
-            if(result != 0){
-                System.out.println(result);
-            }
+                break;
+        }
+        printResult(result);
+    }
+
+    private static void printResult(int result) {
+        if (result != 0) {
+            System.out.println(result);
         }
     }
-}
 
+    public static boolean checkIfOperatorIsCorrect(String operator) {
+        List<String> operators = Arrays.asList("+", "-", "*", "/");
+        if (operators.stream().anyMatch(op -> op.equals(operator))) {
+            return true;
+        } else {
+            System.out.println("Your parameters is not correct");
+            return false;
+        }
+    }
+
+    public static boolean checkIfParametersAreNumbers(String string) {
+        if (string == null) {
+            System.out.println("Your input is null");
+        }else {
+            try {
+                Integer.parseInt(string);
+                return true;
+            } catch (NumberFormatException e) {
+                System.out.println("Your parameter is not a number");
+                System.exit(0);
+                return false;
+            }
+        }
+        return false;
+    }
+}
